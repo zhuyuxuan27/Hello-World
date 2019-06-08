b9c9be33912f14e475091ce2f966a92b1f0a10a1
zhuyuxuan <zhu.yuxuan@live.com>
2019-06-08 18:31:44 -0400

    add two python script

---
+++ draw.py
@@ -0,0 +1,8 @@
+import turtle
+
+painter = turtle.Turtle()
+for _ in range(18):
+    painter.forward(100)
+    painter.right(100)
+
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,7 @@
+
+import turtle
+painter = turtle.Turtle()
+for _ in range(18):
+    painter.forward(100)
+    painter.left(100)
+turtle.done()
